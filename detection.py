13453b53e8ff607f43fa5bfe7bae7b184da4cbfb
Tomtom <pichard.theophile4959@mail.kyutech.jp>
2026-07-20 17:34:45 +0900

    Add all possible classes

--- detection.py
+++ detection.py
@@ -1,20 +1,44 @@
 from ultralytics import YOLO
 from pathlib import Path
 from dataclasses import dataclass
+from typing import ClassVar
 
-"""
-[
-    'apple', 'banana', 'blue berry', 'bread', 'brinjal', 'butter',
-    'cabbage', 'capsicum', 'carrot', 'cheese', 'chicken', 'chocolate',
-    'corn', 'cucumber', 'egg', 'flour', 'fresh cream', 'ginger', 'green beans',
-    'green chilly', 'green leaves', 'lemon', 'meat', 'milk', 'mushroom',
-    'potato', 'shrimp', 'stawberry', 'sweet potato', 'tomato'
-]
-"""
 
 @dataclass
 class PredictionResult:
-    pass
+    results = {
+        'apple': 0, 
+        'banana': 0,
+        'blue berry': 0,
+        'bread': 0,
+        'brinjal': 0,
+        'butter': 0,
+        'cabbage': 0,
+        'capsicum': 0,
+        'carrot': 0,
+        'cheese': 0,
+        'chicken': 0,
+        'chocolate': 0,
+        'corn': 0,
+        'cucumber': 0,
+        'egg': 0,
+        'flour': 0,
+        'fresh cream': 0,
+        'ginger': 0,
+        'green beans': 0,
+        'green chilly': 0,
+        'green leaves': 0,
+        'lemon': 0,
+        'meat': 0,
+        'milk': 0,
+        'mushroom': 0,
+        'potato': 0,
+        'shrimp': 0,
+        'stawberry': 0,
+        'sweet potato': 0,
+        'tomato': 0
+    }
+    
 
 
 class Model:
